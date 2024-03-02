e999459209dc30d186b383c2752870a7dc29dfff
subhamgazzete <159029907+subhamsi22@users.noreply.github.com>
2024-03-02 07:13:12 +0530

    Update pratition_number.c

--- pratition_number.c
+++ pratition_number.c
@@ -1,1 +1,18 @@
-/
+#include<stdio.h>
+void main(){
+    int i,s=0,f,q,n,r;
+    printf("enter the number : ");
+    scanf("%d",&n);
+    q =n;
+    while(q>0){
+        r = q%10;
+        f = 1;
+        for(i=1;i<=r;i++){
+            f =f*i;
+        }
+        s=s+f;
+        q = q/10;
+        
+    }
+    (s==n)?printf("this is a prtation number "):printf("this is not a pratition number ");
+}
